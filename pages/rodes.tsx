c170bd1d6b8353b8975ffdffdeb024a5f65a49e5
Marko Pekas <45564032+MarkoPekas@users.noreply.github.com>
2022-04-15 15:29:13 +0200

    for bebi again

---
+++ pages/rodes.tsx
@@ -0,0 +1,15 @@
+const Rodes = () => {
+    return (
+        <div className="text-center p-4">
+            <p className="text-lg  text-gray-900">Evoo, izaberi si koju hoćeš 😉</p>
+            <p className='text-gray-400 pb-2'>Ili uzmi sve!</p>
+            <img className="w-full shadow-md" src={'/cute.jpg'} />
+            <p className="text-lg py-2 text-gray-900">Ova prva je malo photoshopana, izbacio sam osobu haha</p>
+            <img className="w-full shadow-md mb-2" src={'/1.jpg'} />
+            <img className="w-full shadow-md" src={'/2.jpg'} />
+            <p className="text-lg py-2 text-gray-900">Sve slike su 4k btw</p>
+        </div>
+    )
+}
+
+export default Rodes
